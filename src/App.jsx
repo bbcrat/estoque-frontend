ec64508f1cd142b3f830950736816e7c16c49d66
Bruno Jeronimo <brunojeronimo@MacBook-Air-de-Bruno.local>
2026-05-22 12:47:19 -0300

    Atualiza projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,37 @@ import "./styles/global.css";
 
 function App() {
   return (
-    <div>
-      <h1>Sistema de Estoque</h1>
+    <div className="layout">
+      <aside className="sidebar">
+        <h2>Estoque</h2>
+
+        <nav>
+          <a href="#">Dashboard</a>
+          <a href="#">Produtos</a>
+          <a href="#">Adicionar Produto</a>
+        </nav>
+      </aside>
+
+      <main className="content">
+        <h1>Sistema de Estoque</h1>
+
+        <div className="cards">
+          <div className="card">
+            <h3>Produtos</h3>
+            <p>120</p>
+          </div>
+
+          <div className="card">
+            <h3>Baixo Estoque</h3>
+            <p>8</p>
+          </div>
+
+          <div className="card">
+            <h3>Vendas Hoje</h3>
+            <p>25</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
